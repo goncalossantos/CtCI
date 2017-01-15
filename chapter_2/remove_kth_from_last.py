327dd7b13482a40ec2b32ae6951dec7d5fdfc998
goncalossantos <goncass15@gmail.com>
2017-01-15 15:48:19 +0000

    this exercise is wrong, add todo

--- chapter_2/remove_kth_from_last.py
+++ chapter_2/remove_kth_from_last.py
@@ -1,13 +1,22 @@
 from chapter_2.linked_list import LinkedList
 
 
+# Todo: Redo this because it is return, not remove kth from last
+
+
 def remove_kth_from_last(linked_list: LinkedList, index_to_remove: int) -> None:
-    fast = slow = linked_list.head
-    for _ in range(0, index_to_remove + 1):
-        if not fast:
-            raise Exception("List to small")
-        fast = fast.next
-    while fast and fast.next:
-        slow = slow.next
-        fast = fast.next
-    slow.next = slow.next.next
+    runner = current = linked_list.head
+
+    index = 0
+    while runner and index < index_to_remove:
+        runner = runner.next
+        index += 1
+
+    if not runner:
+        raise Exception("List is not long enough")
+
+    while runner:
+        runner = runner.next
+        current = current.next
+
+    return current.value
--- chapter_2/tests/test_remove_kth_from_last.py
+++ chapter_2/tests/test_remove_kth_from_last.py
@@ -4,13 +4,16 @@ from chapter_2.linked_list import LinkedList
 from chapter_2.remove_kth_from_last import remove_kth_from_last
 
 
-class TestRemove_kth_from_last(TestCase):
+# Todo: Redo this because it is return, not remove kth from last
+
+
+class TestRemoveKthFromLast(TestCase):
     def test_remove_kth_from_last(self):
         test_cases = [
-            ([1, 2, 3, 4], 1, [1, 2, 4]),
-            ([1, 2, 3, 4], 2, [1, 3, 4]),
-            ([1, 2, 3, 4], 3, [2, 3, 4]),
-            ([1, 2, 3, 4], 0, [1, 2, 3]),
+            ([1, 2, 3, 4], 2, [1, 2, 4]),
+            ([1, 2, 3, 4], 3, [1, 3, 4]),
+            ([1, 2, 3, 4], 4, [1, 3, 4]),
+            ([1, 2, 3, 4], 1, [1, 2, 3]),
         ]
 
         for test in test_cases:
