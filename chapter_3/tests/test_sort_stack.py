6bd2f27e6748d357bc4f99a5a767fff874306ed2
goncalossantos <goncass15@gmail.com>
2017-01-16 19:56:52 +0000

    Add sort stack solution with a test

---
+++ chapter_3/tests/test_sort_stack.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from chapter_3.data_structure.stack import Stack
+from chapter_3.sort_stack import sort_stack
+
+
+class TestSort_stack(TestCase):
+    def test(self):
+        stack = Stack()
+        for i in [4, 3, 6, 1, 9, 2, 7, 5, 8]:
+            stack.push(i)
+        sorted_stack = sort_stack(stack)
+        ls = list()
+        while not sorted_stack.is_empty():
+            ls.append(sorted_stack.pop())
+        assert ls == [1, 2, 3, 4, 5, 6, 7, 8, 9]
