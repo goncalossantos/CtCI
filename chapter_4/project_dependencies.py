6baacff4d74b5638b9befe340e60e65db8287c6d
Goncalo Santos <goncalo@reincubate.com>
2017-01-21 22:29:31 +0000

    Add types and docs

--- chapter_4/project_dependencies.py
+++ chapter_4/project_dependencies.py
@@ -1,14 +1,25 @@
 from collections import deque
-from typing import List
+from typing import List, Set
 
-from data_structures.graphs.base import Graph
+from data_structures.graphs.base import Graph, Edge
 
 
-def check_dependencies(dependencies, finished_projects):
+def check_dependencies(dependencies: List[Edge], finished_projects: Set) -> bool:
+    """ Checks if all the dependencies for a project are in finished projects
+
+    :param dependencies: Dependencies of a project
+    :param finished_projects: set with all the finished projects so far
+    :return: returns True if all are satisfied, False if they aren't
+    """
     return all((d.destination in finished_projects for d in dependencies))
 
 
 def handle_dependencies(projects_graph: Graph) -> List[int]:
+    """ Returns a possible order to perform the projects based on their dependencies.
+
+    :param projects_graph:
+    :return: Returns the order in which to perform the projects
+    """
     project_queue = deque(range(projects_graph.v))
     no_advance = 0
 
